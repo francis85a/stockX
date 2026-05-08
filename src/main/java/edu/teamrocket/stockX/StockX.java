2ed09e6f1722ffd26bdd65ff1c8040167ae9adfd
francis85a <franperezcid85@gmail.com>
2026-05-08 13:17:50 +0200

    style(StockX): Cambiar nombre del archivo main

--- src/main/java/edu/teamrocket/stockX/StockX.java
+++ src/main/java/edu/teamrocket/stockX/StockX.java
@@ -3,7 +3,7 @@ package edu.teamrocket.stockX;
 /**
  * Hello world!
  */
-public class App {
+public class StockX {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
